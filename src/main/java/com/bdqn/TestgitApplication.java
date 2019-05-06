39f896e34c68e5ab65ce2d7621ccffb8061fe6ab
ZhangDemoSeven <1273495839@qq.com>
2019-05-06 16:16:26 +0800

    更新了后台和删除

--- src/main/java/com/bdqn/TestgitApplication.java
+++ src/main/java/com/bdqn/TestgitApplication.java
@@ -1,9 +1,11 @@
 package com.bdqn;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.bdqn.mapper")
 public class TestgitApplication {
 
     public static void main(String[] args) {
